d4a36f21c7107a44c71002ec788692feb29f3d28
Hasan <balhaevhk@icloud.com>
2025-05-06 06:39:04 +0300

    new functionality added

--- cmd/guess/main.go
+++ cmd/guess/main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"guess/internal/game"
+	"time"
 )
 
 func main() {
-	game.Game()
+	start := true
+	for start {
+		random, attempts, levelGame := game.StartGame()
+		res, attemptsUsed := game.Game(random, attempts)
+
+		
+		result := game.GameResult{
+			Level:        levelGame,
+			MaxNumber:    random,
+			AttemptsUsed: attemptsUsed,
+			Win:          res,
+			Timestamp:    time.Now().Format("02:01:2006 15:04:05"),
+		}
+
+		if err := game.SaveResult(result); err != nil {
+			fmt.Println("Ошибка при сохранении результата:", err)
+		} else {
+			fmt.Println("Результат игры успешно сохранён!")
+		}
+
+		start = game.GameAgain()
+	}
 }
+
+
+
--- internal/game/game.go
+++ internal/game/game.go
@@ -2,6 +2,8 @@ package game
 
 import (
 	"bufio"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"math/rand"
 	"os"
@@ -10,15 +12,56 @@ import (
 	"time"
 )
 
+type GameResult struct {
+	Level        string    `json:"level"`
+	MaxNumber    int       `json:"maxNumber"`
+	AttemptsUsed int       `json:"attemptsUsed"`
+	Win          bool      `json:"win"`
+	Timestamp    string `json:"timestamp"`
+}
+
 // начало игры
-func Game() {
-	attempts := 10
+func StartGame() (random int, attempts int, levelGame string) {
 	fmt.Println("Вы начали игру \"Угадай число\"")
-	fmt.Println("У вас всего", attempts, "попыток")
-	rand.Seed(time.Now().UnixNano())
-	random := rand.Intn(100) + 1
+	fmt.Println("Для начала выберите номер уровеня:")
+	fmt.Println("1 - easy; 2 - medium; 3 - hard")
+
 	reader := bufio.NewReader(os.Stdin)
-	for  {
+	var number int
+	var err error
+
+	for {
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+		number, err = strconv.Atoi(input)
+		if err != nil {
+			fmt.Println("Введите корректное значение")
+			continue	
+		}
+
+		random, attempts, levelGame, err = Complexity(number)
+		if err != nil {
+			fmt.Println("Ошибка:", err)
+			fmt.Println("Введите один из предложенных уровней")
+			fmt.Println("1 - easy; 2 - medium; 3 - hard")
+			continue
+		}
+		break
+	}
+	return random, attempts, levelGame
+
+}
+
+// начало игры
+func Game(randomNumber int, attempts int) (bool, int) {
+	listAttempts := make([]int, 0)
+	fmt.Println("У вас всего", attempts, "попыток")
+	fmt.Println("Нужно угадать число от 1 до", randomNumber)
+	rand.Seed(time.Now().UnixNano())
+	random := rand.Intn(randomNumber) + 1
+	reader := bufio.NewReader(os.Stdin)
+	var attUsed int
+	for attUsed = 1; attempts > 0; attUsed++ {
 		fmt.Print("Введите ваше число: ")
 		input, _ := reader.ReadString('\n')
 		input = strings.TrimSpace(input)
@@ -28,15 +71,18 @@ func Game() {
 			fmt.Println("Вы ввели не число. Повторите")
 			continue
 		}
+		listAttempts = append(listAttempts, guess)
 		if random < guess {
 			fmt.Println("Секретное число меньше👇")
+			fmt.Println(LastAppempts(listAttempts))
 			Tips(random, guess)
 		} else if random > guess{
 			fmt.Println("Секретное число больше👆")
+			fmt.Println(LastAppempts(listAttempts))
 			Tips(random, guess)
 		} else {
 			fmt.Println("Вы угадали!🙌")
-			return
+			return true, attUsed
 		}
 		attempts--
 		if attempts > 0 {
@@ -46,9 +92,10 @@ func Game() {
 			fmt.Println("Попытки закончились")
 			fmt.Println("Вы проиграли!😢")
 			fmt.Println("А правильный ответ был", random)
-			return
+			return false, attUsed
 		}
 	}
+	return false, attUsed
 }
 
 // подсказки
@@ -66,5 +113,97 @@ func Tips(random int, guess int) {
 	}
 }
 
+// прошлые попытки
+func LastAppempts(attempts []int) string {
+	var s []string
 
+	for _, v := range attempts {
+		s = append(s, strconv.Itoa(v))
+	}
+	result := strings.Join(s, ", ")
+	return fmt.Sprintf("Ранее вы ввели %s", result)
+}
+
+
+// сложность игры
+func Complexity(level int) (random int, attempts int, levelGame string,  err error) {
+	switch level {
+	case 1:
+		random = 50
+		attempts = 15
+		levelGame = "easy"
+	case 2:
+		random = 100
+		attempts = 10
+		levelGame = "medium"
+	case 3:
+		random = 200
+		attempts = 5
+		levelGame = "hurd"
+	default:
+		err = fmt.Errorf("такого уровня нет")
+	}
+	return random, attempts, levelGame, err
+}
+
+// повтор игры
+func GameAgain() (start bool) {
+	fmt.Println("Хотите сыграть? 1 - да; 0 - нет")
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+		s := strings.ReplaceAll(input, " ", "")
+		num, err := strconv.Atoi(s)
+		if err != nil {
+			fmt.Println("Введите 1 или 0")
+			continue
+		}
+		if num == 1 {
+			return  true
+		} else if num == 0 {
+			return false
+		}
+	}
+}
+
+// сохранение результата
+func SaveResult(result GameResult) error {
+	const filePath = "../../storage/results.json"
+
+	var results []GameResult
+
+	// Проверка, существует ли файл и чтение старых данных
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		// Если файл не существует, не выводим ошибку, просто создаем новый с результатами
+		if errors.Is(err, os.ErrNotExist) {
+			fmt.Println("Файл не найден, будет создан новый.")
+		} else {
+			return fmt.Errorf("не удалось прочитать файл: %w", err)
+		}
+	}
+
+	// Если файл существует, пытаемся распарсить его
+	if len(data) > 0 {
+		if err := json.Unmarshal(data, &results); err != nil {
+			return fmt.Errorf("ошибка при чтении JSON: %w", err)
+		}
+	}
+
+	// Добавляем новый результат
+	results = append(results, result)
+
+	// Сохраняем обратно
+	newData, err := json.MarshalIndent(results, "", "  ")
+	if err != nil {
+		return fmt.Errorf("ошибка при маршалинге: %w", err)
+	}
+
+	if err := os.WriteFile(filePath, newData, 0644); err != nil {
+		return fmt.Errorf("не удалось сохранить файл: %w", err)
+	}
+
+	return nil
+}
 
